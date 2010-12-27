b662fd64b245641a79c4482ee423364cda182f90
Dave Jafari <djafaricom@gmail.com>
2010-12-27 03:17:43 -0600

    Spark view now renders correct Last Updated, summary, and title fields

---
+++ app/extensions/StringExtensions.java
@@ -0,0 +1,40 @@
+package extensions;
+
+import play.templates.JavaExtensions;
+import org.apache.commons.lang.StringUtils;
+import com.google.common.collect.Lists;
+import com.google.common.base.Joiner;
+
+import java.util.List;
+
+/**
+ * @author neteller
+ * @created: Dec 27, 2010
+ */
+public class StringExtensions extends JavaExtensions {
+
+  /**
+   * Abbreviates to the maximum length but does not chop word boundaries.
+   * Any markup is cleaned.
+   *
+   * @param s
+   * @param maxLength
+   * @return
+   */
+  public static String abbreviate(String s, int maxLength) {
+    if (StringUtils.isBlank(s) || StringUtils.length(s) <= maxLength) return s;
+
+    List<String> words = Lists.newArrayList();
+    final String[] tokens = s.replaceAll("<p>", "")
+        .replaceAll("</p>", "")
+        .replaceAll("<.*>.*</.*>", "")
+        .split(" ");
+    for (int i = 0; maxLength > 0; i++) {
+      words.add(tokens[i]);
+      maxLength -= tokens[i].length();
+    }
+
+    return Joiner.on(" ").join(words) + "...";
+  }
+
+}
