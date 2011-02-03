7e166e87dc517e60092ae15684f6c2374c13c1aa
Dave Jafari <djafaricom@gmail.com>
2011-02-03 07:48:34 -0600

    Users page

--- app/controllers/Home.java
+++ app/controllers/Home.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.mvc.With;
+import play.Play;
 import filters.AuthorizationFilter;
 import net.sparkmuse.discussion.SparkSearchResponse;
 import net.sparkmuse.discussion.SparkFacade;
@@ -9,8 +10,10 @@ import net.sparkmuse.user.UserFacade;
 import net.sparkmuse.user.Votables;
 import net.sparkmuse.user.UserVotes;
 import net.sparkmuse.data.entity.UserVO;
+import net.sparkmuse.data.entity.UserProfile;
 
 import javax.inject.Inject;
+import java.util.List;
 
 /**
  * Controller for the home page.  The home page is where
@@ -38,6 +41,14 @@ public class Home extends SparkmuseController {
   public static void welcome() {
     final UserVO user = Authorization.getUserFromSession();
     render(user);
+  }
+
+  public static void people() {
+    if (Play.mode == Play.Mode.DEV) {
+      List<UserProfile> profiles = userFacade.getAllProfiles();
+      render(profiles);
+    }
+    index();
   }
 
 }
