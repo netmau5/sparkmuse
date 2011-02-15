5b991d28e156edd560ca848384fc2d0fa9baf6ca
Dave Jafari <djafaricom@gmail.com>
2011-02-15 01:11:33 -0600

    people expertise icons

--- app/net/sparkmuse/data/entity/Expertise.java
+++ app/net/sparkmuse/data/entity/Expertise.java
@@ -5,17 +5,31 @@ package net.sparkmuse.data.entity;
 * @created: Jan 10, 2011
 */
 public enum Expertise {
-  BIG_DATA,
-  BUSINESS,
-  DESIGN,
-  DOMAIN_EXPERTISE,
-  ENGINEERING,
-  FINANCE,
-  LEAN_METHODOLOGY,
-  MARKETING,
-  MOBILE,
-  PRODUCT_DEVELOPMENT,
-  SALES,
-  SEO,
-  SOCIAL_MEDIA;
+  BIG_DATA("Big Data"),
+  BUSINESS("Business"),
+  DESIGN("Design"),
+  DOMAIN_EXPERTISE("Domain Expertise"),
+  ENGINEERING("Engineering"),
+  FINANCE("Finance"),
+  LEAN_METHODOLOGY("Lean Methodology"),
+  MARKETING("Marketing"),
+  MOBILE("Mobile"),
+  PRODUCT_DEVELOPMENT("Product Development"),
+  SALES("Sales"),
+  SEO("SEO"),
+  SOCIAL_MEDIA("Social Media");
+
+  private final String display;
+
+  Expertise(String display) {
+    this.display = display;
+  }
+
+  public String toDisplay() {
+    return display;
+  }
+
+  public String toCss() {
+    return "expertise-" + this.toString().toLowerCase().replace("_", "-");
+  }
 }
